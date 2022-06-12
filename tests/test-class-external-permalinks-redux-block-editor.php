b7aa7f4ea78952e5f3d389a2bfb013bf3a8cf784
Erick Hitter <git-contrib@ethitter.com>
2022-06-11 21:07:46 -0700

    Scaffold unit tests

---
+++ tests/test-class-external-permalinks-redux-block-editor.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Test block-editor integration.
+ *
+ * @pacakge External_Permalinks_Redux
+ */
+
+/**
+ * Class TestClassExternalPermalinksReduxBlockEditor.
+ *
+ * @coversDefaultClass External_Permalinks_Redux_Block_Editor
+ */
+class TestClassExternalPermalinksReduxBlockEditor extends WP_UnitTestCase {
+	/**
+	 * @covers ::register_meta()
+	 */
+	public function test_register_meta() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @covers ::allow_meta_updates()
+	 */
+	public function test_allow_meta_updates() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+
+	/**
+	 * @covers ::enqueue()
+	 */
+	public function test_enqueue() {
+		$this->markTestIncomplete( 'This test has not been implemented.' );
+	}
+}
